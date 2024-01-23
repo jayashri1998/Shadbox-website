4c4ede535148c1f7ebb32c57aafdc1ab2c1330ab
jayashri1998 <jayashrimatale28@gmail.com>
2024-01-23 18:58:59 +0530

    change approuter file

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,67 +1,60 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
-import Home from './components/Home/Home'
-import Career from './Pages/Career'
-import Service from './Pages/Service'
-import Contact from './Pages/Contact'
-import About from './Pages/About'
-import HireDeveloper from './Pages/HireDeveloper'
-import Blog from './Pages/Blog'
-import BlogSection from './components/Blog/BlogSection'
-import MobileApp from './components/Service/MobileApp'
-import WebDevelopment from './components/Service/WebDevelopment'
-import HireSoftware from './components/Service/HireSoftware'
-import Integration from './components/Service/Integration&Migration'
-import UXDesigner from './components/Service/UXDesigner'
-import CareerDetails from './components/Career/CareerDetails'
-import PrivacyPolicy from './components/OtherFile/PrivacyPolicy'
-import Approach from './components/About/Approach'
-import Solution from './components/About/Solution'
-import Success from './components/About/Success'
-import Industries from './components/About/Industries'
-import Signup from './Pages/Signup'
+import Home from './components/Home/Home';
+import Career from './Pages/Career';
+import Service from './Pages/Service';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+import HireDeveloper from './Pages/HireDeveloper';
+import Blog from './Pages/Blog';
+import BlogSection from './components/Blog/BlogSection';
+import MobileApp from './components/Service/MobileApp';
+import WebDevelopment from './components/Service/WebDevelopment';
+import HireSoftware from './components/Service/HireSoftware';
+import Integration from './components/Service/Integration&Migration';
+import UXDesigner from './components/Service/UXDesigner';
+import CareerDetails from './components/Career/CareerDetails';
+import PrivacyPolicy from './components/OtherFile/PrivacyPolicy';
+import Approach from './components/About/Approach';
+import Solution from './components/About/Solution';
+import Success from './components/About/Success';
+import Industries from './components/About/Industries';
 import Login from './Pages/Login';
-import SigningUp from './Pages/SigningUp'
-import CreateCareer from './components/Career/CreateCareer'
-import CreateBlog from './components/Blog/CreateBlog'
- 
-
+import SigningUp from './Pages/SigningUp';
+import CreateCareer from './components/Career/CreateCareer';
+import CreateBlog from './components/Blog/CreateBlog';
 
 const AppRouter = () => {
   return (
-
-   <Router basename='/'>
-  
-   <Routes>
-   <Route path="/" element={<Home/>}/>
-   <Route path="/service" element={<Service/>}/>
-   <Route path="/about" element={<About/>}/>
-   <Route path="/career" element={<Career/>}/>
-   <Route path="/contact" element={<Contact/>}/>
-   <Route path='/blog' element={<Blog/>}/>
-   <Route path='/hiredeveloper' element={<HireDeveloper/>}/>
-   <Route path='/blogsection/:id' element={<BlogSection/>}/>
-   <Route path ='/service/mobileapp' element={<MobileApp/>}/>
-   <Route path='/service/webdevelopment' element={<WebDevelopment/>}/>
-   <Route path='/service/hiresoftware' element={<HireSoftware/>}/>
-   <Route path= '/service/integration' element={<Integration/>}/>
-   <Route path='/service/uxdesign' element= {<UXDesigner/>}/>
-   <Route path="/career/details/:id" element={<CareerDetails />} />
-   <Route path='/privacypolicy' element={<PrivacyPolicy/>}/>
-   <Route path='/about/approach' element={<Approach/>}/>
-   <Route path='/about/solution' element={<Solution/>}/>
-   <Route path='/about/success' element={<Success/>}/>
-   <Route path='/about/industries' element={<Industries/>}/>
-{   /*<Route path ='/signup' element={<Signup/>}/>*/}
-   <Route path = '/login' element ={<Login/>}/>
-   <Route path='/signingup' element={<SigningUp/>}/>
-   <Route path ='/createcareer' element={<CreateCareer/>}/>
- <Route path ='/createblogs' element={<CreateBlog/>}/>
-    </Routes>
-   
-   </Router>
-
-  )
+    <Router basename='/'>
+      <Routes>
+        <Route path="/" render={() => <Home />} />
+        <Route path="/service" render={() => <Service />} />
+        <Route path="/about" render={() => <About />} />
+        <Route path="/career" render={() => <Career />} />
+        <Route path="/contact" render={() => <Contact />} />
+        <Route path='/blog' render={() => <Blog />} />
+        <Route path='/hiredeveloper' render={() => <HireDeveloper />} />
+        <Route path='/blogsection/:id' render={() => <BlogSection />} />
+        <Route path='/service/mobileapp' render={() => <MobileApp />} />
+        <Route path='/service/webdevelopment' render={() => <WebDevelopment />} />
+        <Route path='/service/hiresoftware' render={() => <HireSoftware />} />
+        <Route path='/service/integration' render={() => <Integration />} />
+        <Route path='/service/uxdesign' render={() => <UXDesigner />} />
+        <Route path="/career/details/:id" render={() => <CareerDetails />} />
+        <Route path='/privacypolicy' render={() => <PrivacyPolicy />} />
+        <Route path='/about/approach' render={() => <Approach />} />
+        <Route path='/about/solution' render={() => <Solution />} />
+        <Route path='/about/success' render={() => <Success />} />
+        <Route path='/about/industries' render={() => <Industries />} />
+        <Route path='/login' render={() => <Login />} />
+        <Route path='/signingup' render={() => <SigningUp />} />
+        <Route path='/createcareer' render={() => <CreateCareer />} />
+        <Route path='/createblogs' render={() => <CreateBlog />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default AppRouter
+export default AppRouter;
